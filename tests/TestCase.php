edb4c31dad3cfc3d1aa19f09f8c092afa34679d7
Guillaume Briday <guillaumebriday@gmail.com>
2017-12-30 11:12:18 +0100

    Adding tests for jwt authentication

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Return an user with the anakin state
+     * @return User
+     */
+    protected function anakin($overrides = [])
+    {
+        return factory(User::class)->states('anakin')->create($overrides);
+    }
 }
