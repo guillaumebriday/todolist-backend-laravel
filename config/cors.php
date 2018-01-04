b69a963e36a85a42e35e2dcd9e153e351d92ec2c
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-05 00:51:07 +0100

    Adding Cors configuration file

---
+++ config/cors.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+     * A cors profile determines which orgins, methods, headers are allowed for
+     * a given requests. The `DefaultProfile` reads its configuration from this
+     * config file.
+     *
+     * You can easily create your own cors profile.
+     * More info: https://github.com/spatie/laravel-cors/#creating-your-own-cors-profile
+     */
+    'cors_profile' => Spatie\Cors\CorsProfile\DefaultProfile::class,
+
+    /*
+     * This configuration is used by `DefaultProfile`.
+     */
+    'default_profile' => [
+
+        'allow_origins' => [
+            '*',
+        ],
+
+        'allow_methods' => [
+            'POST',
+            'GET',
+            'OPTIONS',
+            'PUT',
+            'PATCH',
+            'DELETE',
+        ],
+
+        'allow_headers' => [
+            'Content-Type',
+            'X-Auth-Token',
+            'Origin',
+            'Authorization',
+            'X-Requested-With',
+        ],
+
+        /*
+         * Preflight request will respond with value for the max age header.
+         */
+        'max_age' => 60 * 60 * 24,
+    ],
+];
