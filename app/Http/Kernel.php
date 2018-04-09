d34fe97f73472fd931fe4a9490477f8dbc7ae523
Guillaume Briday <guillaumebriday@gmail.com>
2018-04-09 20:32:45 +0200

    Adding a middleware to forceJson

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Spatie\Cors\Cors::class,
+        \App\Http\Middleware\ForceJson::class,
     ];
 
     /**
