8f0430ad56b2785c2bb9ccd431a50db9b28bed9e
Guillaume Briday <guillaumebriday@gmail.com>
2018-05-02 00:02:28 +0200

    Updating log channel to work with Slack

--- config/logging.php
+++ config/logging.php
@@ -32,7 +32,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'slack'],
         ],
 
         'single' => [
@@ -53,7 +53,7 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => 'critical',
+            'level' => 'error',
         ],
 
         'syslog' => [
