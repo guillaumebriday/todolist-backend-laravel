87bf5faa8e64cc75c42cf84eb4e372a2b74be5f5
Guillaume Briday <guillaumebriday@gmail.com>
2018-06-12 22:28:28 +0200

    Adding CurrentPassword rule

---
+++ app/Rules/CurrentPassword.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+
+class CurrentPassword implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     */
+    public function passes($attribute, $value): bool
+    {
+        return Hash::check($value, auth()->user()->password);
+    }
+
+    /**
+     * Get the validation error message.
+     */
+    public function message(): string
+    {
+        return trans('validation.current_password');
+    }
+}
